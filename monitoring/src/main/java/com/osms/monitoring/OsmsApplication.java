c3b7d1beb97d2accb63b29cd7186029d60e437bc
david100gom <david100gom@gmail.com>
2018-01-18 00:01:21 +0900

    first commit

---
+++ monitoring/src/main/java/com/osms/monitoring/OsmsApplication.java
@@ -0,0 +1,12 @@
+package com.osms.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsmsApplication.class, args);
+	}
+}
