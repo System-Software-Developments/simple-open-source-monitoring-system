f15ff22947a7ddb8520d6606a7d6067fb224b557
david100gom <david100gom@gmail.com>
2018-02-22 23:28:58 +0900

    Add heartbeat ping test

--- monitoring/src/test/java/com/osms/monitoring/OsmsApplicationTests.java
+++ monitoring/src/test/java/com/osms/monitoring/OsmsApplicationTests.java
@@ -4,6 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +18,29 @@ public class OsmsApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+		try {
+			// Heartbeat Test like ping...
+			CloseableHttpClient client = HttpClientBuilder.create().build();
+			HttpGet request = new HttpGet("https://www.naver.com");
+			HttpResponse response = client.execute(request);
+
+			BufferedReader bufReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+			StringBuilder builder = new StringBuilder();
+
+			String line;
+
+			while ((line = bufReader.readLine()) != null) {
+				builder.append(line);
+				builder.append(System.lineSeparator());
+			}
+
+			System.out.println(builder);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
