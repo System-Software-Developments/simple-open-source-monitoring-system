54ede3266a1ff627c969951a905d2bb66a75d2d1
david100gom <david100gom@gmail.com>
2018-03-17 10:15:01 +0900

    Add test code

---
+++ monitoring/src/test/java/pattern/dip/Lego.java
@@ -0,0 +1,14 @@
+package pattern.dip;
+
+/**
+ * Created by david100gom on 2018. 3. 17.
+ *
+ * Github : https://github.com/david100gom
+ */
+public class Lego extends Toy{
+
+    public String toString() {
+        return "Lego";
+    }
+}
+
---
+++ monitoring/src/test/java/pattern/dip/Toy.java
@@ -0,0 +1,13 @@
+package pattern.dip;
+
+/**
+ * Created by david100gom on 2018. 3. 17.
+ *
+ * Github : https://github.com/david100gom
+ */
+public class Toy {
+
+    public String toString() {
+        return "";
+    }
+}
