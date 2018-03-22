0d683d2ec62abddca3bc8f8e1b9f23b69e4e10f8
DESKTOP-1HNCF6B\David Kim <david100gom@gmail.com>
2018-03-22 18:37:43 +0900

    clean code

--- monitoring/src/main/java/com/osms/monitoring/OsmsApplication.java
+++ monitoring/src/main/java/com/osms/monitoring/OsmsApplication.java
@@ -12,13 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OsmsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		SpringApplication.run(OsmsApplication.class, args);
 		//BaseValidator.start();
-		try {
-			OsMonitor.runOsMonitor();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		OsMonitor.runOsMonitor();
+
 	}
 }
